02ee747942a590428614a1f7772fc2fe22e97fa8
柴田大輔 <dsuke1983@gmail.com>
2018-05-29 12:51:26 +0900

    GoForwardUnitychan
    課題の提出です。宜しくお願いします。

---
+++ GoForwardUnityChan/Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// ブロックとの衝突音
+	private AudioSource audioSource;
+
+	void Start () {
+	}
+	
+	void Update () {
+
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D ( Collision2D other ) {
+
+		if (other.gameObject.name == "ground" || other.gameObject.tag == "Cube") {
+
+			Debug.Log ("接触");
+			audioSource = GetComponent<AudioSource> ();
+			audioSource.Play ();
+
+		}
+	}
+}
